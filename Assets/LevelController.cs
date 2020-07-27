68e209a7cb1a621a0d88e814ec64278613784553
Dan Rosen <drosen@precipart.com>
2020-07-26 20:34:26 -0400

    Initial commit

---
+++ Assets/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    static int _nextLevelIndex = 1;
+    Enemy[] _enemies;
+
+    private void OnEnable() {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    void Update()
+    {
+        foreach(Enemy enemy in _enemies){
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("you killed all enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# unity-MadBirds
+ Angry Birds clone in Unity, practice project via Jason Weimann on YouTube
